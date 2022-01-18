f7011e179efe961efa8051310776784397587715
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-01-18 14:51:12 +0300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
 
