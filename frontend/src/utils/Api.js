c4c5510eaf2b2d32397573de17de81f7332f2afb
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-02-08 13:48:41 +0300

    Add files via upload

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -1,5 +1,5 @@
 const configAPI = {
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://domainname.students.nomoredomains.rocks',
   headers: {
     'Content-Type': 'application/json',
   },
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -49,6 +49,6 @@ class Auth {
   }
 }
 
-const auth = new Auth('http://localhost:3000');
+const auth = new Auth('https://domainname.students.nomoredomains.rocks');
 
 export default auth;
