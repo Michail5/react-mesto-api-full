a38173c111e1bf47d890fa4c97aa460f026044a0
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-02-08 14:13:37 +0300

    Add files via upload

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,8 +2,8 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const allowedCors = [
   'http://localhost:3000',
-  'http://api.domainnames.students.nomoredomains.rocks',
-  'https://api.domainnames.students.nomoredomains.rocks',
+  'http://domainnames.students.nomoredomains.rocks',
+  'https://domainnames.students.nomoredomains.rocks',
 ];
 
 // eslint-disable-next-line consistent-return
