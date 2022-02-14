4482199cb5c6d744870c310f5420f5320e7ec01d
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-02-14 10:41:28 +0300

    Delete backend/errors directory

--- backend/errors/NotAuthError.js
+++
@@ -1,9 +0,0 @@
-class NotAuthError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = 401;
-    this.name = 'NotAuthError';
-  }
-}
-
-module.exports = NotAuthError;
