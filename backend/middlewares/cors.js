48db12bd46b060dbedd352331932ae76b239d765
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-02-15 10:19:43 +0300

    Update cors.js

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -2,7 +2,6 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const allowedCors = [
   'http://localhost:3000',
-  'http://domainname.students.nomoredomains.rocks',
   'https://domainname.students.nomoredomains.rocks',
 ];
 
