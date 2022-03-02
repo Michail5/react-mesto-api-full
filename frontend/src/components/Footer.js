45d6e2b30ceb8af51f9d1aa9fba001eca76f37bb
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-03-02 16:37:21 +0300

    Delete frontend/src directory

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">©️ 2022. Яковлев Михаил</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- frontend/src/components/Login.js
+++
@@ -1,7 +0,0 @@
-import LoginForm from './LoginForm';
-
-function Login({ onLogin }) {
-  return <LoginForm title="Вход" submitBtnText="Войти" onSubmit={onLogin} />;
-}
-
-export default Login;
