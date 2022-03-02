7d151dcef13a37176b1fbe8f91ee5f5b02fbd650
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-03-02 15:40:38 +0300

    Update cors.js

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,23 +6,22 @@ const ALLOWED_CORS = [
   'https://api.domainnames.students.nomoredomains.rocks',
 ];
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
+  const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  const { origin } = req.headers;
-  console.log(origin);
-  console.log(method);
 
-  if (ALLOWED_CORS.includes(origin)) {
+  if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    return res.end();
   }
 
-  next();
+  next(); // пропускаем запрос дальше
 };
