5161dbc2903e20fc15404edf9330933ecda1cc51
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-03-02 13:40:37 +0300

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const { createUser, login, signOut } = require('./controllers/users');
+const { userValidation, loginValidation } = require('./middlewares/validation');
+const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHandler');
+const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+// const cors = require('./middlewares/cors');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+// app.use(cors);
+
+const corsAllowed = [
+  'http://localhost:3000',
+  'https://domainname.students.nomoredomains.rocks',
+  'http://domainname.students.nomoredomains.rocks',
+];
+
+require('dotenv').config();
+
+app.use(
+  cors({
+    credentials: true,
+    origin(origin, callback) {
+      if (corsAllowed.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  }),
+);
+
+app.options('*', cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signin', loginValidation, login);
+app.post('/signup', userValidation, createUser);
+app.delete('/signout', signOut);
+
+app.use(auth);
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use('*', () => {
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+mongoose.connect('mongodb://localhost:27017/mestodb', {});
+
+app.listen(PORT, () => {
+  console.log('Ссылка на сервер');
+  console.log(PORT);
+  console.log(process.env.JWT_SECRET);
+});
