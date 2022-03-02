29f8ae337b6dab11e5ea83441b040563315aee7a
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-03-02 13:36:25 +0300

    Delete backend directory

--- backend/utils/utils.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-useless-escape
-const linkRegExp = /^(http|https):\/\/(www.)?[\w\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]+\.[\w\/]+#?/;
-
-module.exports = {
-  linkRegExp,
-};
