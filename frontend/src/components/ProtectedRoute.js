3d5af462ffb3b0b4d7ba90300a2ae6a7e379c05c
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-03-28 16:51:37 +0300

    Delete frontend/src/components directory

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-
-function ProtectedRoute({ children, redirectTo, loggedIn }) {
-  return loggedIn ? children : <Navigate to={redirectTo} />;
-}
-
-export default ProtectedRoute;
