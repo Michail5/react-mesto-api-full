4a9922df964e87b361a1963760c19c52c61f27d9
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-03-28 16:51:59 +0300

    Delete frontend/src/utils directory

--- frontend/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || 'https://api.domainnames.students.nomoredomains.rocks';
-
-export const AUTORIZATION_PARAMS = {
-  baseRoute: API_ENDPOINT,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-}
-
