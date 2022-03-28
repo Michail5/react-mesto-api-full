9c3f6873ad1a591306ed18daf1b509b5348a1c0c
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-03-28 12:20:36 +0300

    Delete frontend/src/contexts/components directory

--- frontend/src/contexts/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">©️ 2022. Михаил Яковлев</p>
-    </footer>
-  );
-}
-
-export default Footer;
