a53446927ab22e0368a62362f5e3dcfd79149e47
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-03-28 16:46:21 +0300

    Add files via upload

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const auth = (req, res, next) => {
+  const token = req.headers.authorization.replace('Bearer ', '');
+  if (!token) {
+    throw new UnauthorizedError('Необходимо залогиниться');
+  } else {
+    let payload;
+    try {
+      payload = jwt.verify(
+        token,
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+      );
+    } catch (err) {
+      throw new UnauthorizedError('Необходимо залогиниться');
+    }
+    req.user = payload;
+    next();
+  }
+};
+
+module.exports = auth;
---
+++ backend/routes/users.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const { celebrate, Joi, CelebrateError } = require('celebrate');
+const validator = require('validator');
+
+const userRouter = express.Router();
+// контроллер юзера
+const {
+  getUsers, getUser, patchUser, patchAvatar, getMyUser,
+} = require('../controllers/users');
+
+// GET /users — возвращает всех пользователей
+userRouter.get('/', getUsers);
+
+// GET /users/me - возвращает информацию о текущем пользователе
+userRouter.get('/me', getMyUser);
+
+// GET /users/:userId - возвращает пользователя по _id
+userRouter.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24).hex(),
+  }),
+}), getUser);
+
+// PATCH /users/me — обновляет профиль
+userRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(20),
+  }),
+}), patchUser);
+
+// PATCH /users/me/avatar — обновляет аватар
+userRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().custom((value) => {
+      if (!validator.isURL(value, { require_protocol: true })) {
+        throw new CelebrateError('Некорректный URL');
+      }
+      return value;
+    }),
+  }),
+}), patchAvatar);
+
+module.exports = userRouter;
