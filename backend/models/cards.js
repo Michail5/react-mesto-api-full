3a08a0c467881e7e88776e4c3ac4c787872c0838
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-04-01 14:56:51 +0300

    Add files via upload

---
+++ backend/models/cards.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+      message: 'Введена неверная ссылка',
+    },
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
