c7a56c749ffff7fde2b39334d2480348a5324b61
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-06-14 22:59:05 +0300

    Update cors.js

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -24,6 +24,7 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Origin: https://domainname.students.nomoredomains.rocks');
     return res.end();
   }
 
