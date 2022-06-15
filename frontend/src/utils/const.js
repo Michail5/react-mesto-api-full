3ad2a262f54a1254564b26d5b80990f59b4a85d2
Michail5 <83210599+Michail5@users.noreply.github.com>
2022-06-15 23:17:29 +0300

    Update const.js

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,4 +1,4 @@
 // const baseUrl = 'http://localhost:3001/';
-const baseUrl = 'https://api.domainnames.students.nomoredomains.rocks';
+const baseUrl = 'https://api.domainnames.students.nomoredomains.rocks/';
 
 export default baseUrl;
